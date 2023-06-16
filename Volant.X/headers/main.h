3e340a1cee6ee3fc4647de2df04d4ff80810bb8b
nhl-001 <hoang-long.nguyen.1@ens.etsmtl.ca>
2023-06-15 21:47:48 -0400

    Added interrupts priorities defines

--- Volant.X/headers/main.h
+++ Volant.X/headers/main.h
@@ -54,7 +54,59 @@
 #define SPI_I           LATDbits.LATD2
 #define SPI_O           LATDbits.LATD3
 
+//===============================================
+// Timer interrupts priorities and subpriorities
+//===============================================
+#define TIMER1_INTERRUPT_PRIORITY       2         // Used in ChinookLib function
+#define T1_INTERRUPT_PRIORITY           ipl2auto  // Used in ISR
+#define TIMER2_INTERRUPT_PRIORITY       3         // Used in ChinookLib function
+#define T2_INTERRUPT_PRIORITY           ipl3auto  // Used in ISR
+#define TIMER4_INTERRUPT_PRIORITY       1         // Used in ChinookLib function
+#define T4_INTERRUPT_PRIORITY           ipl1auto  // Used in ISR
 
+#define TIMER1_INTERRUPT_SUBPRIORITY    3         // Highest subpriority
+#define TIMER2_INTERRUPT_SUBPRIORITY    0         // Highest subpriority
+#define TIMER4_INTERRUPT_SUBPRIORITY    1         // Highest subpriority
+//===============================================
+
+
+//===============================================
+// UART interrupts priorities and subpriorities
+//===============================================
+#define UART6_INTERRUPT_PRIORITY        1           // Used in ChinookLib function
+#define U6_INTERRUPT_PRIORITY           ipl1auto    // Used in ISR
+
+#define UART6_INTERRUPT_SUBPRIORITY     0           // Lowest subpriority
+//===============================================
+
+
+//===============================================
+// SPI interrupts priorities and subpriorities
+//===============================================
+#define SPI3_INTERRUPT_PRIORITY         2           // Used in ChinookLib function
+#define S3_INTERRUPT_PRIORITY           ipl2auto    // Used in ISR
+
+#define SPI3_INTERRUPT_SUBPRIORITY      1           // Lowest subpriority
+//===============================================
+
+
+//===============================================
+// ADC interrupts priorities and subpriorities
+//===============================================
+#define ADC_INTERRUPT_PRIORITY          1           // Used in ChinookLib function
+#define ADC_INTERRUPT_PRIO              ipl1auto    // Used in ISR
+
+#define ADC_INTERRUPT_SUBPRIORITY       3           // Highest subpriority
+//===============================================
+
+
+//===============================================
+// CAN interrupts priorities and subpriorities
+//===============================================
+#define CAN1_INTERRUPT_PRIORITY          4           // Used in ChinookLib function
+#define CAN1_INT_PRIORITY                ipl4auto    // Used in ISR
+
+#define CAN1_INTERRUPT_SUBPRIORITY       3           // Highest subpriority
 
 /* ***************************** Declarations ******************************* */
 
